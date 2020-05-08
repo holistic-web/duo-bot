f9ce639a6e34a33f701705bbb4e9ea22afa968fb
Dan Bennett <dghbennett47@gmail.com>
2020-05-08 20:46:55 +0100

    refactor google login

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This is the javascript code that will be run on the Raspberry Pi to automate Goo
 To run this code you must complete the following:
 
 - Download the chromedriver for your version of chrome from http://chromedriver.storage.googleapis.com/index.html
-- Turn off 2 step verification for the Google account
+- Turn off 2 step verification and phone login for the Google account
 - Turn on less secure app access to stop Google from blocking Selenium
 - Change username and password to your google account
 
--- src/ChromeClient.js
+++ src/ChromeClient.js
@@ -1,4 +1,5 @@
 const Webdriver = require('selenium-webdriver');
+const Xpaths = require('./Xpaths.json');
 
 function sleep (time) {
     return new Promise((resolve) => setTimeout(resolve, time));
@@ -11,21 +12,48 @@ module.exports = class ChromeClient {
     }
 
     async login(username, password) {
-        await this.webdriver.get('https://accounts.google.com/servicelogin');
-        const usernameField = this.getElement('//*[@id="identifierId"]');
+        await this.goToURL(Xpaths.url.stackOverflowURL);
+        const stackOverflowNextButton = this.getElement(Xpaths.google_login.stackOverflowNextButton);
+        await stackOverflowNextButton.click();
+        const usernameField = this.getElement(Xpaths.google_login.usernameField);
         await usernameField.sendKeys(username);
-        const usernameNextButton = this.getElement('//*[@id="identifierNext"]/span/span');
+        const usernameNextButton = this.getElement(Xpaths.google_login.usernameNextButton);
         await usernameNextButton.click();
-        await sleep(1000);
-        const passwordField = this.getElement('//*[@id="password"]/div[1]/div/div[1]/input');
-        await passwordField.sendKeys(password);
-        const passwordNextButton = this.getElement('//*[@id="passwordNext"]/span');
+
+        const sleepTime=1000;
+        const retries = 30;
+        let success = false;
+        let attempt = 0;
+        while (!success && attempt < retries) {
+            await sleep(sleepTime);
+            try {
+                const passwordField = this.getElement(Xpaths.google_login.passwordField);
+                await passwordField.sendKeys(password);
+                console.log('entered keys');
+                success = true;
+            } catch (err) {
+                if (err.message.startsWith('no such element:')) {
+                    attempt += 1;
+                } else {
+                    throw err;
+                }
+            }
+        }
+        if (!success) throw new Error('Failed to find the password field');
+
+        const passwordNextButton = this.getElement(Xpaths.google_login.passwordNextButton);
         await (await passwordNextButton).click();
+
+        await this.goToURL(Xpaths.url.duoURL);
     }
 
     getElement(xpath) {
         const element = this.webdriver.findElement(Webdriver.By.xpath(xpath));
         return element;
+    }
+
+    async goToURL(url) {
+        await this.webdriver.get(url);
     }
 
 }
